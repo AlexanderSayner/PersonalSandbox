e75277d4334ef8a551cc2e630d13eb021a2846af
Winreal <alexander.ezhakov@nordclan.com>
2025-12-10 14:18:20 +0400

    Java EE dependencies update

--- javaee-graphql-project/src/main/java/com/example/graphql/GraphQLApplication.java
+++ javaee-graphql-project/src/main/java/com/example/graphql/GraphQLApplication.java
@@ -3,18 +3,7 @@ package com.example.graphql;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-import java.util.HashSet;
-import java.util.Set;
-
 // Standard JAX-RS Application class to enable JAX-RS endpoints
 @ApplicationPath("/")
 public class GraphQLApplication extends Application {
-    
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        // The @GraphQLApi annotated classes will be automatically picked up
-        // by the MicroProfile GraphQL implementation in WildFly
-        return classes;
-    }
 }
